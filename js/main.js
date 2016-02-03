06965a20797ae4ba89110468b3677c9bc55c6b01
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-03 11:00:47 -0500

    adding sample code to respond to a keyword
    This area is where we'll put a big switch with all the commands in it

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ bot.startRTM(function(err,bot,payload) {
     throw new Error('Could not connect to Slack');
   }
 });
+
+controller.hears(["keyword","^pattern$"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+  // do something to respond to message
+  // all of the fields available in a normal Slack message object are available
+  // https://api.slack.com/events/message
+  bot.reply(message,'You used a keyword!');
+});
