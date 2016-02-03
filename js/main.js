a0c929e32db9ae5400d56669a7b8f8b166ff6670
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-03 10:51:38 -0500

    Added slack bot key

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var Botkit = require('botkit');
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
-  token: my_slack_bot_token
+  token: "xoxb-20222523825-CKevtcZa4uer4hCOtdEb7iKB"
 })
 bot.startRTM(function(err,bot,payload) {
   if (err) {
